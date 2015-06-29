1bc6d3853987d5e50862ee9a62306b2969f79e89
mike <github@ravuya.com>
2015-06-29 16:47:50 -0600

    adding demo files

--- gifify.py
+++ gifify.py
@@ -6,8 +6,8 @@ class FrameSettings:
     height = 150
     # todo: support more than horizontal pane one day
     start_x = 0
-    y = 0
-    stop_x = 0
+    y = 27
+    stop_x = 80
 
 class SnapPosition:
     x = 0
@@ -16,7 +16,12 @@ class SnapPosition:
     h = 150
 
 def dump_frame(src, frame_settings, path):
-    print 'Dump frame to file TODO'
+    x1 = frame_settings.x
+    y1 = frame_settings.y
+    x2 = frame_settings.x + frame_settings.w
+    y2 = frame_settings.y + frame_settings.h
+    cropped = src.crop((x1, y1, x2, y2))
+    cropped.save(path)
 
 # compute the positions for a pan
 def get_pan_locations(frame_settings, number_of_frames = 10):
@@ -42,7 +47,10 @@ fs.y = 10
 fs.stop_x = 256
 snap_positions = get_pan_locations(fs)
 
-src = Image.open("test.png")
+src = Image.open("resized_cat.jpg")
+
+i = 0
 
 for snap in snap_positions:
-    dump_frame(src, snap + snap_positions.index(snap) + ".png")
+    dump_frame(src, snap, "pic" + str(i) + ".png")
+    i = i + 1
